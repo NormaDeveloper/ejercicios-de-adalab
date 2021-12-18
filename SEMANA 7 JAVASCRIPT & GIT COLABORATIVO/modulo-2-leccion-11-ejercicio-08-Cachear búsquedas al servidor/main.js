adfb70d302339da80231a521d6c1182be0933f22
Norma <normarivasmadrid@gmail.com>
2021-12-18 17:01:10 +0100

    11.8

---
+++ SEMANA 7 JAVASCRIPT & GIT COLABORATIVO/modulo-2-leccion-11-ejercicio-08-Cachear búsquedas al servidor/main.js
@@ -0,0 +1,28 @@
+'use strict';
+//Sobre el ejercicio 2 vamos a cachear las búsquedas al servidor. De forma que cuando  busquemos una cadena a través del campo de búsqueda, primero busque en localStorage si ya tenemos un resultado en local para esa cadena. Si no lo hay se pide al servidor y luego se guarda en localStorage usando como clave el texto de la búsqueda; si al buscarlo en localStorage lo encontramos pues le enseñamos el resultado directamente al usuario y nos evitamos una petición al servidor.
+
+const list = document.querySelector('.js_characters');
+const searchInput = document.querySelector('.js_searchInput');
+const btn = document.querySelector('.js_btn');
+
+function handleGetCharacter() {
+  if (localStorage.getItem(searchInput.value) !== null) {
+    list.innerHTML = JSON.parse(localStorage.getItem(searchInput.value));
+    console.log('Recupero el dato de la caché');
+  } else {
+    fetch(`https://swapi.py4e.com/api/people/?search=${searchInput.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const characters = data.results;
+        let listData = ''; //limpia lista cada vez que vuelvo a buscar
+        for (let i = 0; i < characters.length; i++) {
+          listData += `<li>Nombre: ${characters[i].name} Género: ${characters[i].gender} </li>`;
+        }
+        console.log(listData);
+        list.innerHTML = listData;
+      });
+
+    localStorage.setItem(searchInput.value, JSON.stringify(listData));
+  }
+}
+btn.addEventListener('click', handleGetCharacter);
